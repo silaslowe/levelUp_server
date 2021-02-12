8273721ac02e6f5375849e0024b5413ea331e714
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-02-12 11:39:17 -0600

    fixted the event model

--- levelup-server/level-up.session.sql
+++ levelup-server/level-up.session.sql
@@ -4,3 +4,15 @@ SELECT * FROM authtoken_token;
 SELECT * FROM levelupapi_gamer;
 SELECT * FROM levelupapi_game;
 SELECT * FROM levelupapi_event;
+
+DELETE 
+FROM levelupapi_event
+WHERE id = 2;
+DELETE 
+FROM levelupapi_event
+WHERE id = 5;
+DELETE 
+FROM levelupapi_event
+WHERE id = 6;
+
+DROP TABLE levelupapi_event;
